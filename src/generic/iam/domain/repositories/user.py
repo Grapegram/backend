83905879f5b91fe36a532b4b2ce5d5769f90349b
Blackgolyb <andrejomelnickij@gmail.com>
2025-11-19 16:36:39 +0200

    feat: add new specific get functions for UserRepository

--- src/generic/iam/domain/repositories/user.py
+++ src/generic/iam/domain/repositories/user.py
@@ -1,7 +1,20 @@
+from typing import Protocol
+
+from returns.maybe import Maybe
+
 from seedwork.domain.repositories import Repository
 
 from ..entities.user import User
 from ..value_objects.user_id import UserId
 
 
-class UserRepository(Repository[UserId, User]): ...
+class UserRepository(Repository[UserId, User], Protocol):
+    """
+    Repository for User aggregate.
+
+    Extends base Repository with user-specific query methods.
+    """
+
+    async def get_by_email(self, email: str) -> Maybe[User]: ...
+
+    async def get_by_username(self, username: str) -> Maybe[User]: ...
