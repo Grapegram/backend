6679702098c688a38038e5abb20859662d98e83f
Blackgolyb <andrejomelnickij@gmail.com>
2025-11-19 16:37:55 +0200

    feat: add token service

---
+++ src/generic/iam/application/services/token.py
@@ -0,0 +1,19 @@
+from typing import Protocol
+
+from returns.result import Result
+
+from seedwork.exceptions import FormattedError
+
+
+class TokenExpiredError(FormattedError):
+    _msg_fmt: str = "Token has expired."
+
+
+class InvalidTokenError(FormattedError):
+    _msg_fmt: str = "Token is invalid."
+
+
+class TokenService(Protocol):
+    def verify(self, token: str) -> Result[dict, TokenExpiredError | InvalidTokenError]: ...
+
+    def generate(self, payload: dict) -> str: ...
