095459dfb6b85dfbd7bc507f613a95e00caeda7f
Blackgolyb <andrejomelnickij@gmail.com>
2025-11-23 01:28:23 +0200

    feat(iam): add send varification email on UserCreated event

---
+++ src/generic/iam/application/handlers/__init__.py
@@ -0,0 +1,5 @@
+from .send_verification_email import SendVerificationEmail
+
+handlers = [SendVerificationEmail]
+
+__all__ = ["handlers"]
---
+++ src/generic/iam/application/handlers/send_verification_email.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+from seedwork.application.handlers import Handler
+from src.generic.iam.application.services.email import EmailService
+from src.generic.iam.application.services.user_token import (
+    UserTokenService,
+    VerificationTokenPayload,
+    VerificationTokenStrategy,
+)
+from src.generic.iam.domain.events import UserCreated
+
+
+@dataclass
+class SendVerificationEmail(Handler):
+    handled = UserCreated
+
+    # Dependencies to be injected
+    user_token_service: UserTokenService
+    email_service: EmailService
+
+    async def handler(self, event: UserCreated) -> None:
+        verification_token = self.user_token_service.create(
+            VerificationTokenStrategy,
+            VerificationTokenPayload(user_id=str(event.user_id), email=event.email),
+        )
+        await self.email_service.send_verification_email(
+            email=event.email,
+            username=event.username,
+            token=verification_token,
+        )
