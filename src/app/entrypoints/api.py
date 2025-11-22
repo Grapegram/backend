5c82eb90d33d31424a14ceb5d76050a153351a2b
Blackgolyb <andrejomelnickij@gmail.com>
2025-11-23 01:37:11 +0200

    feat(app): setup faststream with api

--- src/app/entrypoints/api.py
+++ src/app/entrypoints/api.py
@@ -1,19 +1,25 @@
 from contextlib import asynccontextmanager
 
 import uvicorn
-from dishka.integrations.litestar import DishkaRouter, setup_dishka
+from dishka.integrations.faststream import setup_dishka as fs_setup_dishka
+from dishka.integrations.litestar import setup_dishka as ls_setup_dishka
+from faststream.redis import RedisBroker
 from litestar import Litestar
 from litestar.config.cors import CORSConfig
 from litestar.logging.config import LoggingConfig
 from litestar.openapi.config import OpenAPIConfig
-from litestar.openapi.plugins import RedocRenderPlugin, StoplightRenderPlugin
+from litestar.openapi.plugins import StoplightRenderPlugin
 
+from seedwork.application.event_bus import EventBus
 from src.app.di import create_container
 from src.app.settings import get_settings
 from src.generic.iam.presentation.api import routes as iam_routes
+from src.generic.iam.presentation.events import routes as iam_event_handlers
 
 
 def configure_app() -> Litestar:
+    async_container = create_container()
+
     cors_config = CORSConfig(
         allow_origins=[
             "http://localhost:5174",
@@ -30,9 +36,10 @@ def configure_app() -> Litestar:
         title="Grapegram API",
         version="0.0.0",
         render_plugins=[
-            RedocRenderPlugin(
-                js_url="https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js"
-            )
+            StoplightRenderPlugin(),
+            # RedocRenderPlugin(
+            #     js_url="https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js"
+            # )
         ],
     )
 
@@ -42,22 +49,32 @@ def configure_app() -> Litestar:
         log_exceptions="always",
     )
     route_handlers = [*iam_routes]
-    router = DishkaRouter("", route_handlers=route_handlers)
+    event_handlers = [*iam_event_handlers]
 
     @asynccontextmanager
     async def lifespan(app: Litestar):
+        container = app.state.dishka_container
+        redis_broker: RedisBroker = await container.get(RedisBroker)
+        fs_setup_dishka(container=container, broker=redis_broker)
+
+        event_bus: EventBus = await container.get(EventBus)
+        event_bus.include_routes(event_handlers)
+        await event_bus.start()
+
         yield
-        await app.state.dishka_container.close()
+        await event_bus.stop()
+        await container.close()
 
     app = Litestar(
         lifespan=[lifespan],
-        route_handlers=[router],
+        route_handlers=[*route_handlers],
         on_app_init=[],
         cors_config=cors_config,
         openapi_config=openapi_config,
         logging_config=logging_config,
     )
-    setup_dishka(container=create_container(), app=app)
+
+    ls_setup_dishka(container=async_container, app=app)
     return app
 
 
