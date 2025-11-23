80627ba374ec35c80c5d0135be27c5ca3bdaf738
Blackgolyb <andrejomelnickij@gmail.com>
2025-11-23 15:54:41 +0200

    fix(seedwork): fix tag generation for Handlable

--- seedwork/handler.py
+++ seedwork/handler.py
@@ -3,14 +3,22 @@ from dataclasses import dataclass
 from seedwork.utils.string import camel_to_snake
 
 
-@dataclass(frozen=True)
-class Handlable:
-    __prefix__ = "handlable"
-
+class HandlableMeta(type):
     @classmethod
-    def _create_tag(cls) -> str:
-        return f"{cls.__prefix__}__{camel_to_snake(cls.__name__)}"
+    def _create_tag(cls, cls_instance) -> str:
+        return f"{cls_instance.__prefix__}__{camel_to_snake(cls_instance.__name__)}"
 
-    def __init_subclass__(cls) -> None:
-        if not hasattr(cls, "__tag__"):
-            cls.__tag__ = cls._create_tag()
+    def __new__(cls, name, bases, namespace):
+        new_cls = type.__new__(cls, name, bases, namespace)
+        new_cls.__base_tag__ = cls._create_tag(new_cls)
+        tag = namespace.get("__tag__", None)
+        if isinstance(tag, str):
+            new_cls.__tag__ = tag
+        else:
+            new_cls.__tag__ = new_cls.__base_tag__
+        return new_cls
+
+
+@dataclass(frozen=True)
+class Handlable(metaclass=HandlableMeta):
+    __prefix__ = "handlable"
