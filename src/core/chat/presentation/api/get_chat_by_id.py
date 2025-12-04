72cc71c4a8656df99177901d22bc95615b94363c
Blackgolyb <andrejomelnickij@gmail.com>
2025-12-04 03:09:01 +0200

    feat: add is typing to get chat endpoints

--- src/core/chat/presentation/api/get_chat_by_id.py
+++ src/core/chat/presentation/api/get_chat_by_id.py
@@ -16,6 +16,7 @@ class ChatMemberResponse(Struct):
     id: str
     user_id: str
     role: str
+    is_typing: bool
 
 
 class ChatResponse(Struct):
@@ -61,6 +62,7 @@ async def get_chat_by_id(
                 id=member.id,
                 user_id=member.user_id,
                 role=member.role,
+                is_typing=member.is_typing,
             )
             for member in chat.members
         ],
